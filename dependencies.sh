4ff5ae875a77ec97868f4a116bc9268d1f5ab11d
dwasint <82520990+dwasint@users.noreply.github.com>
2022-07-11 20:17:41 -0700

    changes Zanden wanted

--- dependencies.sh
+++ dependencies.sh
@@ -11,7 +11,7 @@ export BYOND_MINOR=1583
 export RUST_VERSION=1.54.0
 
 #rust_g git tag
-export RUST_G_VERSION=0.4.7.1
+export RUST_G_VERSION=0.7.0
 
 #node version
 export NODE_VERSION=12
@@ -21,7 +21,7 @@ export NODE_VERSION_PRECISE=12.22.4
 export SPACEMAN_DMM_VERSION=suite-1.7.1
 
 #auxmos version
-export AUXMOS_VERSION=v1.1.1
+export AUXMOS_VERSION=v1.1.2
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.9.10
